fde5d610dc1d46cc78f49793650b37677f1bbefe
Arielmakovoz <arielmakovoz@gmail.com>
2023-08-25 12:36:00 -0400

    test

--- api/index.py
+++ api/index.py
@@ -3,8 +3,13 @@ from flask import Flask, request, jsonify
 #hiiiiiii
 app = Flask(__name__)
 
+def say_hello():
+    return 'hello'
+
 @app.route('/api/echo', methods=['POST'])
 def echo():
     data = request.json
     word = data.get('word')
-    return jsonify({'echoed_word': word})
+    return jsonify({'echoed_word': say_hello()})
+
+#when I took out the backend.py from api and stutter model if worked
