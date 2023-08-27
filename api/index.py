e9cc83c5215d79c64a0d0ccbb66bcc684128d153
Arielmakovoz <arielmakovoz@gmail.com>
2023-08-27 14:39:26 -0400

    adding audio

--- api/index.py
+++ api/index.py
@@ -1,15 +1,44 @@
 from flask import Flask, request, jsonify
-#from backend import delete_this
-#hiiiiiii
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
+import wave
+
 app = Flask(__name__)
+CORS(app)
 
-def say_hello():
-    return 'hello'
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/api/echo', methods=['POST'])
-def echo():
-    data = request.json
-    word = data.get('word')
-    return jsonify({'echoed_word': say_hello()})
+@app.route('/api/process_audio', methods=['POST'])
+def process_audio():
+    if 'audio' not in request.files:
+        return jsonify({'error': 'No audio file provided'}), 400
+
+    audio_file = request.files['audio']
+    if audio_file.filename == '':
+        return jsonify({'error': 'No selected audio file'}), 400
+
+    if audio_file:
+        audio_path = os.path.join(app.config['UPLOAD_FOLDER'], audio_file.filename)
+        audio_file.save(audio_path)
+
+        audio_duration = get_audio_duration(audio_path)
+        os.remove(audio_path)  # Remove the temporary audio file
+
+        return jsonify({'duration': audio_duration})
+
+    return jsonify({'error': 'Unknown error'}), 500
+
+def get_audio_duration(audio_path):
+    audio = wave.open(audio_path, 'rb')
+    duration = float(audio.getnframes()) / audio.getframerate()
+    audio.close()
+    return duration
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 #when I took out the backend.py from api and stutter model if worked
+#Lucas wil get the Replicator to work and I will add
+#we will downgrade down to hobby plan (or I will do it myself) and add santiagos new code
