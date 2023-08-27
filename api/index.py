722c2fd379514dd17b0ec098029bd00d1fc271f1
Arielmakovoz <arielmakovoz@gmail.com>
2023-08-27 16:45:36 -0400

    another one

--- api/index.py
+++ api/index.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
-import os
+from flask_cors import CORS
 import wave
 
 app = Flask(__name__)
+CORS(app)
+
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
 @app.route('/api/process_audio', methods=['POST'])
 def process_audio():
@@ -13,18 +16,13 @@ def process_audio():
     if audio_file.filename == '':
         return jsonify({'error': 'No selected audio file'}), 400
 
-    audio_path = 'uploads/' + audio_file.filename
-    audio_file.save(audio_path)
-
-    audio_duration = get_audio_duration(audio_path)
-    os.remove(audio_path)
-
+    audio_duration = get_audio_duration(audio_file)
+    
     return jsonify({'duration': audio_duration})
 
-def get_audio_duration(audio_path):
-    audio = wave.open(audio_path, 'rb')
-    duration = float(audio.getnframes()) / audio.getframerate()
-    audio.close()
+def get_audio_duration(audio_file):
+    with wave.open(audio_file, 'rb') as audio:
+        duration = float(audio.getnframes()) / audio.getframerate()
     return duration
 #when I took out the backend.py from api and stutter model if worked
 #Lucas wil get the Replicator to work and I will add
