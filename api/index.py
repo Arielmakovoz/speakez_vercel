29d175c977aca71b10731fa6978098f01fd5e8da
Arielmakovoz <arielmakovoz@gmail.com>
2023-08-27 18:46:21 -0400

    chcking type of audio

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-
+import audioread
 import wave
 
 app = Flask(__name__)
@@ -14,14 +14,14 @@ def process_audio():
     if audio_file.filename == '':
         return jsonify({'error': 'No selected audio file'}), 400
 
-    audio_duration = get_audio_duration(audio_file)
+    audio_type = get_audio_type(audio_file)
+    if audio_type != 'audio/wav':
+        return jsonify({'error': 'Unsupported audio file type'}), 400
     
-    return jsonify({'duration': audio_duration})
+def get_audio_type(audio_file):
+    with audioread.audio_open(audio_file) as f:
+        return f.mime_type
 
-def get_audio_duration(audio_file):
-    with wave.open(audio_file, 'rb') as audio:
-        duration = float(audio.getnframes()) / audio.getframerate()
-    return duration
 #when I took out the backend.py from api and stutter model if worked
 #Lucas wil get the Replicator to work and I will add
 #we will downgrade down to hobby plan (or I will do it myself) and add santiagos new code
